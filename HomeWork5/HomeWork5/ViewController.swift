64a6ecfde81562db346c10ef56c216c87b331d4d
Kater8 <161327401+Kater8@users.noreply.github.com>
2024-03-12 21:21:23 +0200

    the task is completed

--- HomeWork5/HomeWork5/ViewController.swift
+++ HomeWork5/HomeWork5/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enter: UIButton!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+       super.viewDidLoad()
+        var enterFrame = enter.frame
+     //   enterFrame.size.width =
+
+   }
 
 
 }
